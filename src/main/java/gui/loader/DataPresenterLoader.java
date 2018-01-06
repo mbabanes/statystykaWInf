9a386e8d16fe57e9295fc4f1778970f3a220375d
Michał Grzesik <scyzoryk03@gmail.com>
2018-01-06 12:07:39 +0100

    Refaktoring pod nowy tab dla korelacji.

--- src/main/java/gui/loader/DataPresenterLoader.java
+++ src/main/java/gui/loader/DataPresenterLoader.java
@@ -1,14 +1,14 @@
 package gui.loader;
 
 import entity.provinces.Province;
+import gui.controllers.CorrelationController;
 import gui.controllers.StatisticsController;
 import gui.controllers.WykresController;
+import gui.model.CorrelationModel;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -19,6 +19,8 @@ public class DataPresenterLoader
 {
     private WykresController wykresController;
     private StatisticsController statisticsController;
+    private CorrelationController correlationController;
+
     public static Stage stage;
     private ObservableList<Province> provinces;
 
@@ -30,7 +32,7 @@ public class DataPresenterLoader
     }
 
 
-    public void loadWykresWindow()
+    public void loadMapTab()
     {
         stage = new Stage();
         stage.setTitle("Wykresik");
@@ -39,13 +41,15 @@ public class DataPresenterLoader
         loader.setLocation(this.getClass().getResource("/fxml/wykres.fxml"));
         Pane pane = null;
 
-        try {
+        try
+        {
             pane = loader.load();
-        } catch (IOException e) {
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
-//        Node node = (Node) pane;
-//        tabPane = node.f
+
 
         tabPane = (TabPane) pane.lookup("#tabPane");
         wykresController = loader.getController();
@@ -56,28 +60,41 @@ public class DataPresenterLoader
         wykresController.dajWykres();
 
         Scene sc = new Scene(pane);
-        System.out.println(sc.getWidth());
         stage.setScene(sc);
     }
 
 
-    public void loadStatisticWindow()
+    public void loadStatisticTab()
     {
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/statistic.fxml"));
+        statisticsController = new StatisticsController(provinces);
+        this.tabPane.getTabs().get(1).setContent(loadFXML("/fxml/statistic.fxml", statisticsController));
+    }
+
+
+    public void loadCorrelationTab()
+    {
+        CorrelationModel correlationModel = new CorrelationModel(provinces);
+
+        this.correlationController = new CorrelationController(correlationModel);
+        this.tabPane.getTabs().get(2).setContent(loadFXML("/fxml/correlation.fxml", correlationController));
+    }
+
+    private Pane loadFXML(String fxml, Object controller)
+    {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource(fxml));
+        loader.setController(controller);
         try
         {
-            statisticsController = new StatisticsController(provinces);
-            loader.setController(statisticsController);
-            VBox pane = loader.load();
-//            statisticsController.initialize();
-            pane.setFillWidth(true);
-            this.tabPane.getTabs().get(1).setContent(pane);
-
+            VBox vbox = loader.load();
+//            vbox.setFillWidth(true);
+            return vbox;
         }
         catch (IOException e)
         {
-            System.out.println("Blad ladowania statisitcWindow");
+            e.printStackTrace();
         }
+
+        return null;
     }
 
     public WykresController getWykresController()
